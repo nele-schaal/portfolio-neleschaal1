1bbc6419381bdacaccf6c51ac27e8765e54f2f32
nele-schaal <nele.schaal@hfg.design>
2025-03-12 11:30:16 +0100

    Initial commit: Set up Nuxt 3 project with TypeScript, Tailwind CSS, and basic structure

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <Header />
+    <NuxtPage />
+    footer
+  </div>
+</template>
+
+<script>
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/about">About</NuxtLink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+a {
+  color: green;
+  padding-right: 20px;
+}
+a:hover {
+  font-weight: bold;
+}
+</style>
