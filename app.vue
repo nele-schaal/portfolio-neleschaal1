6e60faf6091327c0b6674173145c3a833cf924f9
nele-schaal <nele.schaal@hfg.design>
2025-05-23 16:57:16 +0200

    Update favicon assets: replace existing favicon SVG with new PNG and ICO files, and add additional favicon links for improved compatibility and user experience.

--- app.vue
+++ app.vue
@@ -1,7 +1,11 @@
 <template>
   <Head>
     <title>Nele Schaal</title>
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="icon" type="image/png" href="/favicon/favicon-96x96-new.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon/favicon-new.svg" />
+    <link rel="shortcut icon" href="/favicon/favicon-new.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon-new.png" />
+    <link rel="manifest" href="/favicon/site.webmanifest" />
   </Head>
   <div class="min-h-screen bg-white flex flex-col">
     <main class="flex-grow">
